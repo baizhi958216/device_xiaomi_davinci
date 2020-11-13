LineageOS/android_device_xiaomi_davinci

davinci: parts: Add local FileUtils
* We don't really need lineageos sdk for one class

Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7

parts/src/org/lineageos/devicesettings/popupcamera/PopupCameraService.java
@@ -32,6 +32,7 @@ import android.os.SystemClock;
 import android.os.UserHandle;
 import android.util.Log;
 import org.lineageos.devicesettings.R;
+import org.lineageos.devicesettings.utils.FileUtils;
 import vendor.xiaomi.hardware.motor.V1_0.IMotor;
 
 public class PopupCameraService extends Service implements Handler.Callback {

baizhi958216/device_xiaomi_davinci

Signed-off-by: trpsl <trpsl@pixelexperience.org>

parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -33,7 +33,7 @@ import android.util.Log;
 
 import java.util.List;
 
-import org.lineageos.internal.util.FileUtils;
+import org.lineageos.settings.utils.FileUtils;
 import vendor.xiaomi.hardware.motor.V1_0.IMotor;
 
 public class PopupCameraService extends Service {